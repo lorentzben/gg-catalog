d38d5cfda57796879fa2cd34a52e4436c682b93c
Benjamin Lorentz <bjl34716@uga.edu>
2023-01-26 15:33:15 -0500

    update 00_collect_data.sh
    - add wgets for reference genomes for the minimap2 calls

--- code/00_collect_data.sh
+++ code/00_collect_data.sh
@@ -61,3 +61,18 @@ cd colorectum
 prefetch --max-size 100G SRR19683890  SRR19732729 
 fastq-dump --split-3 SRR19683890  SRR19732729
 cd ..
+
+if [[ ! -d /scratch/bjl34716/gg-catalog/refs ]]; then
+    mkdir -p /scratch/bjl34716/gg-catalog/refs
+fi
+
+cd /scratch/bjl34716/gg-catalog/refs
+
+# collect chicken genome from NCBI 
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/016/699/485/GCF_016699485.2_bGalGal1.mat.broiler.GRCg7b/GCF_016699485.2_bGalGal1.mat.broiler.GRCg7b_genomic.fna.gz -O GalGal-reference.fna.gz
+
+# collect maize genome from NCBI
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/902/167/145/GCF_902167145.1_Zm-B73-REFERENCE-NAM-5.0/GCF_902167145.1_Zm-B73-REFERENCE-NAM-5.0_genomic.fna.gz -O ZeaMays-reference.fna.gz
+
+# collect soybean genome from NCBI
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/004/515/GCF_000004515.6_Glycine_max_v4.0/GCF_000004515.6_Glycine_max_v4.0_genomic.fna.gz -O GlycineMax-reference.fna.gz
