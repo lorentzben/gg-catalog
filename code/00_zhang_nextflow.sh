f6b47f8c4cb7885e86baed82282da0c106270dc5
Benjamin Lorentz <bjl34716@uga.edu>
2023-01-27 16:17:21 -0500

    needed to add a \

--- code/00_zhang_nextflow.sh
+++ code/00_zhang_nextflow.sh
@@ -27,7 +27,7 @@ nextflow run lorentzben/gg-catalog-nf \
         -c /home/bjl34716/applegate/villegas/compare_methods/.nextflow/config/gacrc.config \
         -profile slurm,singularity \
         -params-file /home/bjl34716/my_utils/gg-catalog/code/params/test_params.yaml \
+        -latest \
         -resume
-        -latest
         
 
