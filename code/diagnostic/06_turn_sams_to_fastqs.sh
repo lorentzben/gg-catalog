d1cb13b370fbbbe293eee6695f8a8aadf1646252
Benjamin Lorentz <bjl34716@uga.edu>
2023-02-06 10:36:04 -0500

    update 06
    - split piped steps up into saving intermediaries since the resultfiles keep getting printed to console and making huge logfiles

--- code/diagnostic/06_turn_sams_to_fastqs.sh
+++ code/diagnostic/06_turn_sams_to_fastqs.sh
@@ -16,8 +16,14 @@ cd /scratch/bjl34716/nf_dev/gg-catalog/compare-minimap/indv-ref
 
 # create fastq for the individual reads
 
-singularity run docker://lorentzb/samtools samtools sort /scratch/bjl34716/nf_dev/gg-catalog/compare-minimap/indv-ref/merged_reads.sam | singularity run docker://lorentzb/samtools samtools view -f 4 | singularity run docker://lorentzb/samtools samtools fastq -1 merged-clean.fastq.gz
+singularity run docker://lorentzb/samtools samtools sort /scratch/bjl34716/nf_dev/gg-catalog/compare-minimap/indv-ref/merged_reads.sam -o sorted_merged.sam
+singularity run docker://lorentzb/samtools samtools view -f 4 sorted_merged.sam -o viewed_sorted_merged.sam 
+singularity run docker://lorentzb/samtools samtools fastq -1 merged-clean.fastq.gz -n viewed_sorted_merged.sam
 
 cd /scratch/bjl34716/nf_dev/gg-catalog/compare-minimap/concat-ref
 
-singularity run docker://lorentzb/samtools samtools sort /scratch/bjl34716/nf_dev/gg-catalog/compare-minimap/concat-ref/SRR15214153.sam | singularity run docker://lorentzb/samtools samtools view -f 4 | singularity run docker://lorentzb/samtools samtools fastq -1 concat-db-clean.fastq.gz
+singularity run docker://lorentzb/samtools samtools sort /scratch/bjl34716/nf_dev/gg-catalog/compare-minimap/concat-ref/SRR15214153.sam -o sorted_concat.sam
+singularity run docker://lorentzb/samtools samtools view -f 4 sorted_concat.sam -o viewed_sorted_concat.sam
+singularity run docker://lorentzb/samtools samtools fastq -1 concat-db-clean.fastq.gz -n viewed_sorted_concat.sam
+
+# TODO rm -rf *.sam 
