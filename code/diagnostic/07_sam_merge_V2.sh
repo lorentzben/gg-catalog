c4cff620c145854095baf9e7559fd638d345d842
Benjamin Lorentz <bjl34716@uga.edu>
2023-02-06 11:07:09 -0500

    add 07_merge
    - try to merge samfiles again, but combine headers with colliding IDs

---
+++ code/diagnostic/07_sam_merge_V2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#SBATCH --partition=batch
+#SBATCH --job-name=07_MERGE_SAMS_V2
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --nodes=1
+#SBATCH --time=96:00:00
+#SBATCH --mem=32gb
+
+#Replace this with your UGA email to get notified on completion
+#SBATCH --mail-user="bjl34716@uga.edu"
+#SBATCH --mail-type=BEGIN,END
+
+cd /scratch/bjl34716/nf_dev/gg-catalog/compare-minimap/indv-ref
+
+# These are needed if 05 was not performed
+# sort gal-gal
+
+#singularity run docker://lorentzb/samtools samtools sort /scratch/bjl34716/nf_dev/gg-catalog/compare-minimap/indv-ref/gal-gal/SRR15214153_gal_gal.sam -o /scratch/bjl34716/nf_dev/gg-catalog/compare-minimap/indv-ref/gal-gal/SRR15214153_gal_gal_sorted.sam
+
+# sort glycine-max
+
+#singularity run docker://lorentzb/samtools samtools sort /scratch/bjl34716/nf_dev/gg-catalog/compare-minimap/indv-ref/glycine-max/SRR15214153_glycine_max.sam -o /scratch/bjl34716/nf_dev/gg-catalog/compare-minimap/indv-ref/glycine-max/SRR15214153_glycine_max_sorted.sam
+
+# sort zea-mays
+
+#singularity run docker://lorentzb/samtools samtools sort /scratch/bjl34716/nf_dev/gg-catalog/compare-minimap/indv-ref/zea-mays/SRR15214153_zea_mays.sam -o /scratch/bjl34716/nf_dev/gg-catalog/compare-minimap/indv-ref/zea-mays/SRR15214153_zea_mays_sorted.sam
+
+# merge sams
+
+singularity run docker://lorentzb/samtools samtools merge -c merged_reads_V2.sam */*_sorted.sam 
+
+
